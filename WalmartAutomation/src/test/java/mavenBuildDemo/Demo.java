25df22437a3e632c7274fe5efb4a6dc293feff52
abdul <abdul@DESKTOP-TPQAGRT>
2018-04-25 23:18:46 -0500

    One message is added.

---
+++ WalmartAutomation/src/test/java/mavenBuildDemo/Demo.java
@@ -0,0 +1,15 @@
+package mavenBuildDemo;
+
+import org.testng.annotations.Test;
+
+public class Demo {
+	
+	
+	@Test
+	public void demoTest() {
+		
+		System.out.println("Welcome to the world of Maven!");
+		System.out.println("Git is very good option for development projects!");
+	}
+
+}
